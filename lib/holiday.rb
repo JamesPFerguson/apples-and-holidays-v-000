125ed62136021a68fd731de39d4887bd2069ce8a
James Ferguson <ncjamesferguson@gmail.com>
2017-12-07 21:50:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,5 +76,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  arr = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |supplies|
 
 end
